26c12c88b51a81ab12c8076166aaab16f3cb810a
flash76 <flash76@users.noreply.github.com>
2019-11-15 05:54:44 -0500

    Update gitignore, check for internet connection now

--- launcher.py
+++ launcher.py
@@ -8,6 +8,27 @@ from sys import stdout
 from bs4 import BeautifulSoup
 
 
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+        return t
+
+
+def check_internet_connection():
+    try:
+        urllib.request.urlretrieve("https://checkra.in")
+        print("Connection succeeded. Continuing.")
+        return True
+    except:
+        print("Could not establish connection to https://checkra.in.")
+        return False
+
+
 def get_local_version():
     if os.path.isdir("checkra1n.app"):
         with open("checkra1n.app/Contents/Info.plist", 'rb') as fp:
@@ -62,11 +83,30 @@ def download_checkra1n():
             unpack_dmg()
 
 
+def guide_dfu():
+    print("DFU Assistant: Sending device to recovery mode...")
+    uuid = str(os.popen("ideviceinfo | grep UniqueDeviceID").readlines()).split(" ")[1]
+    uuid = uuid.strip("\\n\']")
+    os.system("ideviceenterrecovery " + uuid)
+    print("DFU Assistant: Your device should be in recovery mode. It should have a cable\npointing to a computer or "
+          "iTunes icon. If not, exit and try again. Prepare to hold power and home in 3 seconds.")
+    time.sleep(3)
+    for x in range(1, 10):
+        print("DFU Assistant: Hold power and home for " + countdown(10-x) + " seconds.")
+    for x in range(1, 10):
+        print("DFU Assistant: now just hold home for " + countdown(10-x) + " seconds")
+    os.system("checkra1n.app/Contents/MacOS/checkra1n_gui -")
+
+
 if __name__ == "__main__":
     if get_local_version() != get_remote_version():
-        print("Downloading newest version...")
-        download_checkra1n()
+        if check_internet_connection():
+            print("Downloading newest version...")
+            download_checkra1n()
+            guide_dfu()
+        else:
+            print("Unable to download checkra1n. Connect to a different network and try again.")
     else:
         print("Newest version already downloaded...")
     print("Update done. Running checkra1n...\n")
-    os.system("checkra1n.app/Contents/MacOS/checkra1n_gui -")
+    guide_dfu()
